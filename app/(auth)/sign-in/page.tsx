b2b2baf2bb5339825343bfdc184c1f489e4c5666
huberrt5671 <hubernotis34@gmail.com>
2024-12-10 08:46:32 +0900

    implement initial file and folder structure

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const SignIn = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      SIGN IN
+    </section>
+  )
+}
+
+export default SignIn
---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <section className="flex-center size-full max-sm:px-6">
+      SIGN UP
+    </section>
+  )
+}
+
+export default SignUp
